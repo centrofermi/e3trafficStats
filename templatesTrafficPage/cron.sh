51dc46ad41b670f92b18b1968a70f20aab046ac8
Francesco Noferini <noferini@bo.infn.it>
2017-07-12 10:23:31 +0200

    fix

--- templatesTrafficPage/cron.sh
+++ templatesTrafficPage/cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd ~/traffic
+cd ~/e3trafficStats
 cp /home/noferini/traffic/*.png .
 cp /home/noferini/traffic/trafficSummary.txt .
-cp ../templatesTrafficPage/index.html .
-cat trafficSummary.txt |grep -v total|awk '{print "../templatesTrafficPage/addEff.sh",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}'|bash
+cp ./templatesTrafficPage/index.html .
+cat trafficSummary.txt |grep -v total|awk '{print "./templatesTrafficPage/addEff.sh",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}'|bash
 
 export TIME=$(date)
 sed  -i "s/TIME_TEMP/$TIME/g" index.html
