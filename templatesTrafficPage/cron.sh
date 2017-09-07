7290a70efcdd35c1887f4847fdcb1e918fc72fe7
Francesco Noferini <noferini@bo.infn.it>
2017-09-07 15:11:26 +0200

    minor fix

--- templatesTrafficPage/cron.sh
+++ templatesTrafficPage/cron.sh
@@ -6,6 +6,6 @@ cp ./templatesTrafficPage/index.html .
 cp trafficSummary.txt trafficSummary.tmp
 cat trafficSummary.tmp |grep -v total|awk '{print "./templatesTrafficPage/addEff.sh",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}'|bash
 
-export TIME=$(date)
+export TIME=$(date "+%A %d %B %Y")
 sed  -i "s/TIME_TEMP/$TIME/g" index.html
 
