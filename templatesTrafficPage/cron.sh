f2d8defbcf137a4112ad48fb6e6ab2cd104571a0
Francesco Noferini <noferini@bo.infn.it>
2017-09-07 15:01:47 +0200

    formatting accrodingly to eee web style

--- templatesTrafficPage/cron.sh
+++ templatesTrafficPage/cron.sh
@@ -3,7 +3,8 @@ cd ~/e3trafficStats
 cp /home/noferini/traffic/*.png .
 cp /home/noferini/traffic/trafficSummary.txt .
 cp ./templatesTrafficPage/index.html .
-cat trafficSummary.txt |grep -v total|awk '{print "./templatesTrafficPage/addEff.sh",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}'|bash
+cp trafficSummary.txt trafficSummary.tmp
+cat trafficSummary.tmp |grep -v total|awk '{print "./templatesTrafficPage/addEff.sh",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}'|bash
 
 export TIME=$(date)
 sed  -i "s/TIME_TEMP/$TIME/g" index.html
